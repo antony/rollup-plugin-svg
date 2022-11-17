ed8f617965a0727df58b970b5a90e99879ebf53a
Jack Westbrook <jack.westbrook@gmail.com>
2022-11-17 10:18:20 +0100

    feat: migrate to typescript, update dependencies for rollup v3

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { extname } from "path";
+import { Plugin } from "rollup";
+import { createFilter, type FilterPattern } from "@rollup/pluginutils";
+
+function toDataUrl(code: string) {
+  const mime = "image/svg+xml";
+  const buffer = Buffer.from(code, "utf-8");
+  const encoded = buffer.toString("base64");
+  return `'data:${mime};base64,${encoded}'`;
+}
+
+interface RollupSvgOptions {
+  include?: FilterPattern;
+  exclude?: FilterPattern;
+  base64?: boolean;
+}
+
+export default function svg(options: RollupSvgOptions = {}): Plugin {
+  const filter = createFilter(options.include, options.exclude);
+
+  return {
+    name: "svg",
+
+    transform(code: string, id: string) {
+      if (!filter(id) || extname(id) !== ".svg") {
+        return null;
+      }
+
+      const content = code.trim();
+      const encoded = options.base64
+        ? toDataUrl(content)
+        : JSON.stringify(content);
+
+      return { code: `export default ${encoded}`, map: { mappings: "" } };
+    },
+  };
+}
